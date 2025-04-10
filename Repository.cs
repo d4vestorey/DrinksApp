4f802510591ce82c2e51476b58aceac290c7f520
d4vestorey <75077258+d4vestorey@users.noreply.github.com>
2025-04-10 14:49:42 +0100

    Test change

--- Repository.cs
+++ Repository.cs
@@ -7,6 +7,7 @@ public record class Repository(
     [property: JsonPropertyName("strDrink")] string DrinkName,
     [property: JsonPropertyName("idDrink")] string DrinkID,
     [property: JsonPropertyName("strInstructions")] string DrinkInstructions,
-    [property: JsonPropertyName("strGlass")] string GlassType
+    [property: JsonPropertyName("strGlass")] string GlassType,
+    [property: JsonPropertyName("strDrinkThumb")] string DrinkImage
 );
 }
